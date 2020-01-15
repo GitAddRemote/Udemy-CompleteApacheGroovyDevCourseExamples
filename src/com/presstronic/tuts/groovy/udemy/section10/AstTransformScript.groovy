22967b553dc8709bdf88939db9f08e3d1ce85615
Demian <demian@presstronic.com>
2020-01-15 01:10:09 -0500

    Section 10 exercise files

---
+++ src/com/presstronic/tuts/groovy/udemy/section10/AstTransformScript.groovy
@@ -0,0 +1,22 @@
+package com.presstronic.tuts.groovy.udemy.section10
+
+import groovy.transform.Field
+
+@Field Map cars = [porsche:"911", ferrari:"488 GTB", audi:"R8"]
+
+def orderCarsByMake() {
+    cars.sort{ a, b ->
+        a.key <=> b.key
+    }
+}
+println orderCarsByMake()
+assert orderCarsByMake() == [audi:"R8", ferrari:"488 GTB", porsche:"911"]
+
+
+def orderCarsByModel() {
+    cars.sort{ a, b ->
+        a.value <=> b.value
+    }
+}
+println orderCarsByModel()
+assert orderCarsByModel() == [ferrari:"488 GTB", porsche:"911", audi:"R8"]
